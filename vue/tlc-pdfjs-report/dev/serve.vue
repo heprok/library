4c8ae6d7e148cd10c51221df558d7f2cb5e68ed1
Oleg Kravchuck <ok@techno-les.com>
2021-07-26 10:54:05 +0300

    init project

---
+++ vue/tlc-pdfjs-report/dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import TlcPdfjsReport from '@/tlc-pdfjs-report.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    TlcPdfjsReport
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <tlc-pdfjs-report />
+  </div>
+</template>
